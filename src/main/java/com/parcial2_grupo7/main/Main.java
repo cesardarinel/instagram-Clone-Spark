408df8ccd2f5437598730b08aa98278f70e0eb52
marti <martin26_8@hotmail.com>
2016-06-28 13:19:29 -0400

    *Creacion de los metodos GET de la pagina de inicio, registro, login.

--- src/main/java/com/parcial2_grupo7/main/Main.java
+++ src/main/java/com/parcial2_grupo7/main/Main.java
@@ -1,4 +1,12 @@
 package com.parcial2_grupo7.main;
+import freemarker.template.Configuration;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 /**
@@ -8,6 +16,31 @@ public class Main {
     // copiado del base de la pagina funciona genial 
     public static void main(String[] args) {
 
-        get("/hello", (req, res) -> "Hello World");
+        Spark.staticFileLocation("/Recursos");
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(Main.class, "/template");
+        FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine(configuration);
+
+        get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+
+            //TODO modificar plantilla home/index
+            return new ModelAndView(attributes, "index.ftl");
+        }, freeMarkerEngine);
+
+        get("/login", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+
+            //TODO crear plantilla login
+            return new ModelAndView(attributes, "login.ftl");
+        }, freeMarkerEngine);
+
+        get("/register", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+
+            //TODO crear plantilla registrar
+            return new ModelAndView(attributes, "signup.ftl");
+        }, freeMarkerEngine);
+
     }
 }
